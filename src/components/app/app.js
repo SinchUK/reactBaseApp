1d67d5049b60cbcf35601c849596ed78c60b01c3
Gean <000cuiquesuum000@gmail.com>
2022-02-07 10:10:43 +0300

    Optimization code with Component props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,6 +9,13 @@ import './app.css';
 
 
 function App() {
+
+    const data = [
+        {name: 'John C.', salary: 800, increase: false},
+        {name: 'Alex M.', salary: 3000, increase: true},
+        {name: 'Paul D.', salary: 5000, increase: false},
+    ];
+
     return (
         <div className="app">
             <AppInfo/>
@@ -16,7 +23,7 @@ function App() {
                 <SearchPanel/>
                 <AppFilter/>
             </div>
-            <EmployeesList/>
+            <EmployeesList data={data}/>
             <EmployeesAddForm/>
         </div>
     )
