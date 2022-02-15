8a132843cf49b89f4c105e87e0b680dd5f9b9225
Gean <000cuiquesuum000@gmail.com>
2022-02-15 18:34:26 +0300

    Added filters and search panel functionality

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,14 +1,17 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item"
 import './employees-list.css';
 
-const EmployeesList = ({data, onDelete, onToggleProp}) => {
+const EmployeesList = ({data, onDelete, onToggleProp, onValueChange}) => {
     
     const elements = data.map(item => {
         const {id, ...itemProps} = item;
         return (
             <EmployeesListItem 
-                key={id} {...itemProps}
+                key={id}
+                id={id} 
+                {...itemProps}
                 onDelete={() => onDelete(id)}
+                onValueChange={onValueChange}
                 onToggleProp={(e)=> onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}/>
         )
     })
